bb35276ea58538f05f723fc352450ad10bb24035
Wesley <wangp_yong@163.com>
2018-07-16 18:20:17 +0800

    rabbitmq学习

---
+++ demo-rabbitmq/src/main/java/com/wesley/study/MQReceiver.java
@@ -0,0 +1,30 @@
+package com.wesley.study;
+
+import com.wesley.study.config.MQConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Created by Wesley on 2018/6/25.
+ */
+@Service
+public class MQReceiver {
+    private static Logger log = LoggerFactory.getLogger(MQReceiver.class);
+
+    @RabbitListener(queues=MQConfig.QUEUE_DIRECT)
+    public void receive(String message) {
+        log.info("receive message: {}", message);
+    }
+
+    @RabbitListener(queues= MQConfig.TOPIC_QUEUE1)
+    public void receiveTopic1(String message) {
+        log.info(" topic  queue1 message: {}", message);
+    }
+
+    @RabbitListener(queues=MQConfig.TOPIC_QUEUE2)
+    public void receiveTopic2(String message) {
+        log.info(" topic  queue2 message: {}", message);
+    }
+}
---
+++ demo-rabbitmq/src/main/java/com/wesley/study/MQSender.java
@@ -0,0 +1,31 @@
+package com.wesley.study;
+
+import com.wesley.study.config.MQConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Created by Wesley on 2018/6/25.
+ */
+@Service
+public class MQSender {
+
+    private static Logger log = LoggerFactory.getLogger(MQSender.class);
+
+    @Autowired
+    AmqpTemplate amqpTemplate ;
+
+    public void send(Object message) {
+		log.info("send message: {}", message);
+		amqpTemplate.convertAndSend(MQConfig.QUEUE_DIRECT, message);
+	}
+
+	public void sendTopic(Object message) {
+		log.info("send topic message: {}", message);
+		amqpTemplate.convertAndSend(MQConfig.TOPIC_EXCHANGE, "topic.key1", message+ "1");
+		amqpTemplate.convertAndSend(MQConfig.TOPIC_EXCHANGE, "topic.key2", message+ "2");
+	}
+}
